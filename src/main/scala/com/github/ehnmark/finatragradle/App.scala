42c3038efa7e2d9636dd834ebba52ac6833f22c2
Jacob Ehnmark <git@ehnmark.com>
2014-04-13 21:36:47 +0900

    template + json + route params

--- src/main/scala/com/github/ehnmark/finatragradle/App.scala
+++ src/main/scala/com/github/ehnmark/finatragradle/App.scala
@@ -3,9 +3,25 @@ package com.github.ehnmark.finatragradle
 import com.twitter.finatra._
 import com.twitter.finatra.ContentType._
 
+class MyView(val user: String) extends View {
+	val template = "templates/index.mustache"
+}
+
 class MyController extends Controller {
-	get("/") { request => 
-		render.plain("it works!").toFuture }
+	get("/template/:user") { request => 
+		val user = request.routeParams.getOrElse("user", "anonymous")
+		val view = new MyView(user)
+		render.view(view).toFuture
+	}
+
+	get("/plain") { request => 
+		render.plain("it works!").toFuture
+	}
+
+	get("/json") { request => 
+		val map = Map("apa" -> 1, "bpa" -> 2)
+		render.json(map).toFuture
+	}
 }
 
 object App extends FinatraServer {
